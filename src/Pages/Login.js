ed24d89b4cefbf305eed36bd7ea700ab293ed47d
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-04-27 09:48:19 -0300

    rediseño login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -20,45 +20,42 @@ const Login = () => {
 
     useEffect(() => {
         // Redirige si vuelve al login y ya esta registrado
-        if(login) navigate("/")
+        if (login) navigate("/")
     }, [])
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        if(loader) return
+
         const email = e.target.email.value;
         const password = e.target.password.value;
-
-        setLoader(true)
 
         const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
         if (email === "") {
             setMsgEmail("El email no puede estar vacio")
-            setLoader(false)
             return
         }
         setMsgEmail(false)
         if (password === "") {
             setMsgPassword(true)
-            setLoader(false)
             return
         }
         setMsgPassword(false)
 
         if (email !== "" && !regexEmail.test(email)) {
             setMsgEmail("El email no es valido")
-            setLoader(false);
             return
         }
 
         if (email !== "challenge@alkemy.org" || password !== "react") {
             setMsgLogin(true)
-            setLoader(false);
             return
         }
         setMsgLogin(false)
 
         // si todo esta ok
+        setLoader(true)
         const URL = "http://challenge-react.alkemy.org/";
         axios.post(URL, {
             email: email,
@@ -69,6 +66,7 @@ const Login = () => {
                     localStorage.setItem("token", res.data.token);
                     setLogin(res.data.token)
                     navigate("/")
+                    setLoader(false)
                 }
             })
             .catch(error => {
@@ -78,49 +76,45 @@ const Login = () => {
                     title: 'El server no responde',
                     text: 'hubo un error del lado del servidor, intentelo mas tarde.'
                 })
+                setLoader(false)
             })
-
-        // un reseteo por la dudas
-        setLoader(false);
     }
 
     return (
         <div className='login col'>
-            <img className='logo-form p-5 App-logo' src={logo} alt="" />
-
             <div className='form-container rounded'>
-                {
-                    loader
-                    ?   <>
-                            <Loader />
-                            <h3 className='h3'>Procesando la petición...</h3>
-                            </>
-                    :   <form className='form p-3' onSubmit={handleSubmit}>
-                            <h2 className='py-3'>Formulario de login</h2>
-                            <div className="mb-3">
-                                <label htmlFor="email" className="form-label">Correo electronico</label>
-                                <input id='email' type="email" name="email" className="form-control" autoComplete='current-email' />
-                                {
-                                    msgEmail ? <p className="form-text text-danger">{msgEmail}</p> : null
-                                }
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="password" className="form-label">Contraseña</label>
-                                <input name="password" autoComplete='current-password' type="password" className="form-control" id="password" />
-                                {
-                                    msgPassword ? <p className="form-text text-danger">La contraseña no puede estar vacio</p> : null
-                                }
-                            </div>
-                            <button type="submit" className="btn btn-primary w-100">Submit</button>
-                            {
-                                msgLogin ? <p className="form-text text-danger">No estas registrado</p> : null
-                            }
-                        </form>
-                }
-                <p className='text-center'>
-                    ● Email: challenge@alkemy.org
-                    ● Password: react
-                </p>
+                <div className='row w-100 m-auto'>
+                    {
+                        loader
+                        ? <Loader />
+                        : <img className='logo-form p-1 App-logo' src={logo} alt="" />
+                    }
+                </div>
+
+                <form className='form' onSubmit={handleSubmit}>
+                    <h1 className='h2 mb-3'>Iniciar sesión</h1>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label" >Correo electronico</label>
+                        <input id='email' type="email" name="email" className="form-control" autoComplete='current-email' placeholder='challenge@alkemy.org' tabIndex={loader ? -1 : 0} />
+                        {
+                            msgEmail ? <p className="form-text text-danger">{msgEmail}</p> : null
+                        }
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Contraseña</label>
+                        <input name="password" autoComplete='current-password' type="password" className="form-control" id="password" placeholder='react' tabIndex={loader ? -1 : 0} />
+                        {
+                            msgPassword ? <p className="form-text text-danger">La contraseña no puede estar vacio</p> : null
+                        }
+                    </div>
+                    <button type="submit" className={(loader ? "disabled " : "") + "btn btn-primary w-100"} tabIndex={loader ? -1 : 0}>Submit</button>
+                    {
+                        msgLogin ? <p className="form-text text-danger">No estas registrado</p> : null
+                    }
+                    <p className={(loader ? "open rounded " : "") + 'form-modal h3 text'}>
+                        Cargando ...
+                    </p>
+                </form>
             </div>
         </div>
     )
